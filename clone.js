7c9495444f50d4a2062b0ed774fa11f6ceb1e93c
swk <774932198@163.com>
2021-11-15 10:01:33 +0800

    对象深拷贝性能优化

--- clone.js
+++ clone.js
@@ -14,7 +14,7 @@ function deepClone1(target) {
  * @return {Object}
  */
 function deepClone2(target) {
-    if (typeof target === 'object' && target !== null){
+    if (typeof target === 'object' && target !== null) {
         const result = Array.isArray(target) ? [] : {}
         for (const key in target) {
             if (Object.hasOwnProperty.call(target, key)) {
@@ -22,7 +22,7 @@ function deepClone2(target) {
             }
         }
         return result
-    }else {
+    } else {
         return target
     }
 }
@@ -34,18 +34,51 @@ function deepClone2(target) {
  * @param {Map} hash 
  * @return {Object}
  */
- function deepClone3(target, hash = new Map()) {
-    if (typeof target === 'object' && target !== null){
+function deepClone3(target, hash = new Map()) {
+    if (typeof target === 'object' && target !== null) {
         // 判断是否克隆过
-        if(hash.get(target)){
+        if (hash.get(target)) {
             return hash.get(target)
-        } 
+        }
         const result = Array.isArray(target) ? [] : {}
         hash.set(target, result)
         for (const key in target) {
             if (Object.hasOwnProperty.call(target, key)) {
                 result[key] = deepClone3(target[key], hash);
             }
+        }
+        return result
+    } else {
+        return target
+    }
+}
+
+/**
+ * @description 深拷贝4，递归拷贝方式(利用map,解决循环引用, 区分数组对象分别循环处理性能优化)
+ * @param {Object} target 
+ * @param {Map} hash 
+ * @return {Object}
+ */
+function deepClone4(target, hash = new Map()) {
+    if (typeof target === 'object' && target !== null) {
+        // 判断是否克隆过
+        if (hash.get(target)) {
+            return hash.get(target)
+        }
+        let isArr = Array.isArray(target)
+        
+        const result = isArr ? [] : {}
+        hash.set(target, result)
+        if(isArr) {
+            // 数组的情况下
+            target.forEach((item, index) => {
+                result[index] = deepClone4(item, hash);
+            })
+        } else {
+            // 对象的情况
+            Object.keys(target).forEach((key)=>{
+                result[key] = deepClone4(target[key], hash)
+            })
         }
         return result
     } else {
